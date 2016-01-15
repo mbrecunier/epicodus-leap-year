9c186eb72b0e16e7783ecb17c21b1e1429d94e13
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-15 15:14:51 -0800

    add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
+  return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (result) {
+      $(".not").text("");
+    } else {
+      $(".not").text("not");
+    }
+
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
